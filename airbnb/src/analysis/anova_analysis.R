cd73f67ada84e763e091905369d17dced50436e3
Claudia Berkhof <Claudia@Claudias-MacBook-Pro-2.local>
2022-03-11 16:48:55 +0100

    added anova's with moderators

--- airbnb/src/analysis/anova_analysis.R
+++ airbnb/src/analysis/anova_analysis.R
@@ -7,28 +7,50 @@ library(car)
 library(effectsize)
 
 ## Import the cleaned data
-setwd("../../data")
+setwd("../gen/analysis/input")
 data_airbnb_uk_cleaned <- read.csv("data_airbnb_uk_cleaned.csv")
 
-# Charactarize independent variables: levels
+# Charactarize independent variables
 data_airbnb_uk_cleaned$quarter <- as.factor(data_airbnb_uk_cleaned$quarter)
 
-## Anova with just quarter as independent variable.
-anova_price <- lm(price ~ quarter, data_airbnb_uk_cleaned)
-Anova(anova_price, type = 3)
+## Anova with quarter as independent variable and price, review_scores_rating_rescaled, and calculated_host_listings_count as dependent variables
+anova1 <- lm(price ~ quarter, data_airbnb_uk_cleaned)
+Anova(anova1, type = 3)
 
-anova_review_score <- lm(review_scores_rating_rescaled ~ quarter, data_airbnb_uk_cleaned)
-Anova(anova_review_score, type = 3)
+anova2 <- lm(review_scores_rating_rescaled ~ quarter, data_airbnb_uk_cleaned)
+Anova(anova2, type = 3)
 
-anova_listings <- lm(calculated_host_listings_count ~ quarter, data_airbnb_uk_cleaned)
-Anova(anova_listings, type = 3)
+anova3 <- lm(num_host_listings ~ quarter, data_airbnb_uk_cleaned)
+Anova(anova3, type = 3)
 
-# Effect size
+# Effect size for the Anova's
 eta_squared(anova_price, ci=0.95, partial = TRUE)
 
 eta_squared(anova_review_score, ci=0.95, partial = TRUE)
 
 eta_squared(anova_listings, ci=0.95, partial = TRUE)
+
+#Moderation effect of neighbourhood (be patient when loading the moderators)
+mod1 <- aov(price ~ quarter*neighbourhood_name, data = data_airbnb_uk_cleaned)
+summary(mod1)
+
+mod2 <- aov(review_scores_rating_rescaled ~ quarter*neighbourhood_name, data = data_airbnb_uk_cleaned)
+summary(mod2)
+
+mod3 <- aov(num_host_listings ~ quarter*neighbourhood_name, data = data_airbnb_uk_cleaned)
+summary(mod3)
+     
+#Save Anova output
+capture.output(Anova(anova1, type = 3),  file = "../output/anova1_airbnb.doc")
+capture.output(Anova(anova2, type = 3),  file = "../output/anova2_airbnb.doc")
+capture.output(Anova(anova3, type = 3),  file = "../output/anova3_airbnb.doc")
+capture.output(summary(mod1),  file = "../output/mod1_airbnb.doc")
+capture.output(summary(mod2),  file = "../output/mod2_airbnb.doc")
+capture.output(summary(mod3),  file = "../output/mod3_airbnb.doc")
+
+
+#The above part can be kept, below not
+
 
 ## Anova with quarter and neighbourhood as IV's
 ## Check for multicollinearity between quarter and neighbourhood
