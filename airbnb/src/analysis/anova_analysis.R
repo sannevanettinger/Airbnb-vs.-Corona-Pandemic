9fdf97a680e3ead6fddbcc44cb63a43e7e3ab0d6
jjacobs123 <jurgjacobs@kpnmail.nl>
2022-03-11 17:05:22 +0100

    update anova analysis

--- airbnb/src/analysis/anova_analysis.R
+++ airbnb/src/analysis/anova_analysis.R
@@ -40,7 +40,27 @@ summary(mod2)
 mod3 <- aov(num_host_listings ~ quarter*neighbourhood_name, data = data_airbnb_uk_cleaned)
 summary(mod3)
      
-#Save Anova output
+# Assumptions ANOVA
+# Randomly selecting 5000 rows from the dataset to investigate homoskedasticity and normality
+data_airbnb_uk_cleaned_sample <- sample_n(data_airbnb_uk_cleaned, 5000)
+
+# homoskedasticity
+leveneTest(price ~ quarter*neighbourhood_name, data_airbnb_uk_cleaned_sample)
+leveneTest(review_scores_rating_rescaled ~ quarter*neighbourhood_name, data_airbnb_uk_cleaned_sample)
+leveneTest(num_host_listings ~ quarter*neighbourhood_name, data_airbnb_uk_cleaned_sample)
+
+## all of the values in the Levene's Test are higher than 0.05, so we can not reject the null hypothesis (variances are equal). In other words, there is homoskedasticity for all the variables.
+
+# normality
+shapiro.test(data_airbnb_uk_cleaned_sample$price)
+shapiro.test(data_airbnb_uk_cleaned_sample$review_scores_rating_rescaled)
+shapiro.test(data_airbnb_uk_cleaned_sample$num_host_listings)
+
+## Since all the p-values of the Shapiro-Wilk normality test are lower than 0.05, it can be concluded that the data is NOT normally distributed.
+
+
+#Save Anova & assumptions output
+## Anova
 capture.output(Anova(anova1, type = 3),  file = "../output/anova1_airbnb.doc")
 capture.output(Anova(anova2, type = 3),  file = "../output/anova2_airbnb.doc")
 capture.output(Anova(anova3, type = 3),  file = "../output/anova3_airbnb.doc")
@@ -48,47 +68,12 @@ capture.output(summary(mod1),  file = "../output/mod1_airbnb.doc")
 capture.output(summary(mod2),  file = "../output/mod2_airbnb.doc")
 capture.output(summary(mod3),  file = "../output/mod3_airbnb.doc")
 
+## Anova assumptions
+capture.output(leveneTest(price ~ quarter*neighbourhood_name, data_airbnb_uk_cleaned_sample), file = "../output/levene_test1_airbnb.doc")
+capture.output(leveneTest(review_scores_rating_rescaled ~ quarter*neighbourhood_name, data_airbnb_uk_cleaned_sample), file = "../output/levene_test2_airbnb.doc")
+capture.output(leveneTest(num_host_listings ~ quarter*neighbourhood_name, data_airbnb_uk_cleaned_sample), file = "../output/levene_test3_airbnb.doc")
+capture.output(shapiro.test(data_airbnb_uk_cleaned_sample$price), file = "../shapiro_test1_airbnb.doc")
+capture.output(shapiro.test(data_airbnb_uk_cleaned_sample$review_scores_rating_rescaled), file = "../shapiro_test2_airbnb.doc")
+capture.output(shapiro.test(data_airbnb_uk_cleaned_sample$num_host_listings), file = "../shapiro_test3_airbnb.doc")
 
 #The above part can be kept, below not
-
-
-## Anova with quarter and neighbourhood as IV's
-## Check for multicollinearity between quarter and neighbourhood
-## Note Jurg: it is not possible to investigate the interaction between quarter and neighbourhood on price, review score and listings, since neighbourhood and quarter are non-metric
-HDF_cor <- cor(data_airbnb_uk_cleaned, use="pairwise.complete.obs")
-HDF_cor
-## Werk niet omdat niet alle variabelen in de dataset numeric zijn.
-
-## price
-anova_price_1 <- lm(price ~ quarter+neighbourhood_cleansed, data_airbnb_uk_cleaned)
-Anova(anova_price_1, type = 3)
-
-## review scores
-anova_review_scores_1 <- lm(review_scores_rating_rescaled ~ quarter+neighbourhood_cleansed, data_airbnb_uk_cleaned)
-Anova(anova_review_scores_1, type = 3)
-
-## listings
-anova_listings_1 <- lm(calculated_host_listings_count ~ quarter+neighbourhood_cleansed, data_airbnb_uk_cleaned)
-Anova(anova_listings_1, type = 3)
-
-
-# Effect size
-eta_squared(anova_price_1, ci=0.95, partial = TRUE)
-
-eta_squared(anova_review_score_1, ci=0.95, partial = TRUE)
-
-eta_squared(anova_listings_1, ci=0.95, partial = TRUE)
-
-## Look into the interaction between quarter and neighbourhood. Is it possible?
-
-
-
-# Assumptions ANOVA
-## Homoskedasticity and normality (plots)
-
-
-
-## Results to gen/output folder
-
-
-            
